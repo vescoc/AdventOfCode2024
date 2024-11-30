e1fb4e00574e3c6333e0f11105fa67459d5c4920
Vesco Claudio <claudio.vesco@gmail.com>
2024-11-30 23:49:48 +0100

    preparing for tomorrow...

--- trunk-all.sh
+++ trunk-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for d in day[0-2][0-9]; do (echo ">>> $d"; cd $d/rsui; time trunk build --release --filehash false --public-url /AdventOfCode2023/$d/); done
+for d in day[0-2][0-9]; do (echo ">>> $d"; cd $d/rsui; time trunk build --release --filehash false --public-url /AdventOfCode2024/$d/); done
