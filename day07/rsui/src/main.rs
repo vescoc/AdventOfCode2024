ca561152d53f79a81d6136a85f08deb8c9248159
Vesco Claudio <claudio.vesco@gmail.com>
2024-12-06 22:42:47 +0100

    using aoc, removing input file

--- day07/rsui/src/main.rs
+++ day07/rsui/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let model_props = ui::ModelProps {
-        input: day01::INPUT.to_string(),
-        solve_1: day01::solve_1,
-        solve_2: day01::solve_2,
+        input: day07::INPUT.to_string(),
+        solve_1: day07::solve_1,
+        solve_2: day07::solve_2,
     };
     yew::Renderer::<ui::Model<_, _, _, _>>::with_props(model_props).render();
 }
