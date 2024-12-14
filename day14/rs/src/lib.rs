be13c13bd552c064a3f625cbb1ad6c5599ac5ef3
Vesco Claudio <claudio.vesco@gmail.com>
2024-12-14 23:55:06 +0100

    day14

--- day14/rs/src/lib.rs
+++ day14/rs/src/lib.rs
@@ -1,26 +1,125 @@
+#![no_std]
 #![allow(clippy::must_use_candidate)]
 
+use heapless::{String as HLString, Vec as HLVec};
+
+#[cfg(feature = "input")]
 use lazy_static::lazy_static;
 
+#[cfg(feature = "input")]
 lazy_static! {
     pub static ref INPUT: &'static str = include_str!("../../input");
 }
 
-/// # Panics
-pub fn solve_1(_input: &str) -> u32 {
-    todo!()
+#[cfg(not(feature = "input"))]
+pub const INPUT: &str = "";
+
+pub const WIDTH: i32 = 101;
+pub const HEIGHT: i32 = 103;
+
+type String = HLString<8>;
+type Vec<T> = HLVec<T, 512>;
+
+pub struct Robot {
+    pub position: (i32, i32),
+    pub velocity: (i32, i32),
+}
+
+impl Robot {
+    fn new(position: (i32, i32), velocity: (i32, i32)) -> Self {
+        Self { position, velocity }
+    }
+}
+
+fn parse(data: &str) -> (i32, i32) {
+    let mut parts = data.split(',');
+
+    (
+        parts
+            .next()
+            .unwrap()
+            .chars()
+            .skip(2)
+            .collect::<String>()
+            .parse::<i32>()
+            .unwrap(),
+        parts.next().unwrap().parse::<i32>().unwrap(),
+    )
 }
 
 /// # Panics
-pub fn solve_2(_input: &str) -> u32 {
-    todo!()
+pub fn robots(input: &str) -> impl Iterator<Item = Robot> + use<'_> {
+    input.lines().map(|line| {
+        let mut parts = line.split_whitespace().map(parse);
+        let (px, py) = parts.next().unwrap();
+        let (vx, vy) = parts.next().unwrap();
+
+        Robot::new((px, py), (vx, vy))
+    })
 }
 
-pub fn part_1() -> u32 {
-    solve_1(&INPUT)
+/// # Panics
+#[allow(clippy::cast_sign_loss)]
+pub fn solve_1<const WIDTH: i32, const HEIGHT: i32>(input: &str) -> usize {
+    robots(input)
+        .fold(
+            [0, 0, 0, 0],
+            |mut acc,
+             Robot {
+                 position: (px, py),
+                 velocity: (vx, vy),
+             }| {
+                let (px, py) = (
+                    (px + vx * 100).rem_euclid(WIDTH),
+                    (py + vy * 100).rem_euclid(HEIGHT),
+                );
+
+                if px != WIDTH / 2 && py != HEIGHT / 2 {
+                    let (px, py) = (2 * px / WIDTH, 2 * py / HEIGHT);
+                    acc[px as usize * 2 + py as usize] += 1;
+                }
+
+                acc
+            },
+        )
+        .iter()
+        .product()
 }
 
-pub fn part_2() -> u32 {
+/// # Panics
+pub fn solve_2(input: &str) -> usize {
+    const TARGET: &[u8] = b"**********";
+
+    let robots = robots(input).collect::<Vec<_>>();
+
+    (0..10000)
+        .position(|i| {
+            let mut map = [[b' '; WIDTH as usize]; HEIGHT as usize];
+            for Robot {
+                position: (px, py),
+                velocity: (vx, vy),
+            } in &robots
+            {
+                let (px, py) = (
+                    (px + vx * i).rem_euclid(WIDTH) as usize,
+                    (py + vy * i).rem_euclid(HEIGHT) as usize,
+                );
+                map[py][px] = b'*';
+            }
+
+            map.into_iter()
+                .any(|row| (0..row.len() - TARGET.len()).any(|i| row[i..].starts_with(TARGET)))
+        })
+        .unwrap()
+}
+
+#[cfg(feature = "input")]
+pub fn part_1() -> usize {
+    solve_1::<WIDTH, HEIGHT>(&INPUT)
+}
+
+#[cfg(feature = "input")]
+pub fn part_2() -> usize {
     solve_2(&INPUT)
 }
 
@@ -29,17 +128,22 @@ mod tests {
     use super::*;
 
     lazy_static! {
-        static ref INPUT_1: &'static str = r#""#;
-        static ref INPUT_2: &'static str = r#""#;
+        static ref INPUT: &'static str = r#"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"#;
     }
 
     #[test]
     fn same_results_1() {
-        assert_eq!(solve_1(&INPUT_1), 42);
-    }
-
-    #[test]
-    fn same_results_2() {
-        assert_eq!(solve_2(&INPUT_2), 42);
+        assert_eq!(solve_1::<11, 7>(&INPUT), 12);
     }
 }
--- day14/rsui/src/main.rs
+++ day14/rsui/src/main.rs
@@ -1,8 +1,197 @@
+use std::time::Duration;
+
+use wasm_bindgen::JsCast;
+
+use web_sys::{CanvasRenderingContext2d as C2D, HtmlCanvasElement, HtmlInputElement};
+
+use instant::Instant;
+
+use yew::prelude::*;
+
+use day14::{robots, solve_1, solve_2, Robot, HEIGHT, INPUT, WIDTH};
+
+const ZOOM: f64 = 2.0;
+
+const STEPS: i32 = 1;
+
+#[derive(Properties, PartialEq)]
+pub struct ModelProps {
+    pub input: String,
+}
+
+pub enum Msg {
+    Run(String),
+    Rewind,
+    Forward,
+    Reset,
+}
+
+pub struct Model {
+    input_ref: NodeRef,
+    canvas_ref: NodeRef,
+    part1: Option<usize>,
+    part2: Option<usize>,
+    input: String,
+    elapsed_part_1: Option<Duration>,
+    elapsed_part_2: Option<Duration>,
+    elapsed_total: Option<Duration>,
+    current: Option<i32>,
+}
+
+impl Model {
+    fn render_solution(&self, i: Option<i32>) {
+        let canvas = self.canvas_ref.cast::<HtmlCanvasElement>().unwrap();
+
+        let g: C2D = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+
+        g.clear_rect(0.0, 0.0, WIDTH as f64 * ZOOM, HEIGHT as f64 * ZOOM);
+        if let Some(i) = i {
+            for Robot {
+                position: (px, py),
+                velocity: (vx, vy),
+            } in robots(&self.input)
+            {
+                let (px, py) = (
+                    (px + vx * i).rem_euclid(WIDTH) as f64,
+                    (py + vy * i).rem_euclid(HEIGHT) as f64,
+                );
+
+                g.fill_rect(px * ZOOM, py * ZOOM, ZOOM, ZOOM);
+            }
+        }
+    }
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ModelProps;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let input = ctx.props().input.clone();
+
+        Self {
+            input_ref: NodeRef::default(),
+            canvas_ref: NodeRef::default(),
+            part1: None,
+            part2: None,
+            input,
+            elapsed_part_1: None,
+            elapsed_part_2: None,
+            elapsed_total: None,
+            current: None,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Run(input) => {
+                let now_part_1 = Instant::now();
+                self.part1 = Some(solve_1::<WIDTH, HEIGHT>(&input));
+                self.elapsed_part_1 = Some(now_part_1.elapsed());
+
+                let now_part_2 = Instant::now();
+                self.part2 = Some(solve_2(&input));
+                self.elapsed_part_2 = Some(now_part_2.elapsed());
+
+                self.elapsed_total = Some(now_part_1.elapsed());
+
+                self.input = input;
+
+                self.current = self.part2.map(|v| v as i32);
+
+                self.render_solution(self.current);
+
+                true
+            }
+            Msg::Rewind => {
+                if let Some(i) = self.current.as_mut() {
+                    *i = (*i - STEPS).max(0);
+                }
+
+                self.render_solution(self.current);
+
+                true
+            }
+            Msg::Forward => {
+                if let Some(i) = self.current.as_mut() {
+                    *i = (*i + STEPS).min(20000);
+                }
+
+                self.render_solution(self.current);
+
+                true
+            }
+            Msg::Reset => {
+                if let Some(i) = self.current.as_mut() {
+                    *i = 0;
+                }
+
+                self.render_solution(self.current);
+
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+        let input_ref = self.input_ref.clone();
+
+        let onclick = link.batch_callback(move |_| {
+            let input = input_ref.cast::<HtmlInputElement>();
+            input.map(|input| Msg::Run(input.value()))
+        });
+
+        let rewind = link.callback(move |_| Msg::Rewind);
+        let forward = link.callback(|_| Msg::Forward);
+        let reset = link.callback(|_| Msg::Reset);
+
+        fn format_duration(elapsed: Option<Duration>) -> String {
+            elapsed
+                .map(|v| format!("{}ms ({}us)", v.as_millis(), v.as_micros()))
+                .unwrap_or_else(|| "not run".to_string())
+        }
+
+        html! {
+            <>
+                <label for="input"> { "Input: " }
+            <textarea id="input" ref={self.input_ref.clone()} rows="4" cols="50" value={self.input.clone()} />
+                </label>
+                <button {onclick}>{ "\u{23F5}" }</button>
+                <label for="results"> { "Results: " }
+            <div id="results" class="output">
+                <div class="result"><label> { "Part 1: " } </label> { self.part1 }</div>
+                <div class="result"><label> { "Part 2: " } </label> { self.part2 }</div>
+            </div>
+            <div id="elapsed" class="output">
+                <div class="result"><label> { "Part 1 Elapsed: " } </label> { format_duration(self.elapsed_part_1) }</div>
+                <div class="result"><label> { "Part 2 Elapsed: " } </label> { format_duration(self.elapsed_part_2) }</div>
+                <div class="result"><label> { "Elapsed: " } </label> { format_duration(self.elapsed_total) }</div>
+            </div>
+                </label>
+                <div class="output">
+                <label for="canvas"> { format!("View: {:>8}", self.current.unwrap_or_default()) }
+            <canvas class="output" ref={self.canvas_ref.clone()} width={ ((WIDTH as f64 * ZOOM) as u32).to_string() } height={ ((HEIGHT as f64 * ZOOM) as u32).to_string() }>
+                </canvas>
+                </label>
+                <div>
+                <button onclick={rewind}>{ "<" }</button><button onclick={forward}>{ ">" }</button><button onclick={reset}>{ "R" }</button>
+                </div>
+
+                </div>
+                </>
+        }
+    }
+}
+
 fn main() {
-    let model_props = ui::ModelProps {
-        input: day14::INPUT.to_string(),
-        solve_1: day14::solve_1,
-        solve_2: day14::solve_2,
+    let model_props = ModelProps {
+        input: INPUT.to_string(),
     };
-    yew::Renderer::<ui::Model<_, _, _, _>>::with_props(model_props).render();
+    yew::Renderer::<Model>::with_props(model_props).render();
 }
