60b2cc9db9b09c940d4e3a3ad9453327ce963bd3
Vesco Claudio <claudio.vesco@gmail.com>
2024-12-20 19:19:11 +0100

    day20

--- day20/rs/src/lib.rs
+++ day20/rs/src/lib.rs
@@ -1,26 +1,139 @@
 #![allow(clippy::must_use_candidate)]
 
+use heapless::FnvIndexMap;
+
+use bitset::BitSet;
+
+type Map<K, V> = FnvIndexMap<K, V, { 4096 * 4 }>;
+
+#[cfg(feature = "parallel")]
+use rayon::prelude::*;
+
+#[cfg(feature = "input")]
 use lazy_static::lazy_static;
 
+#[cfg(feature = "input")]
 lazy_static! {
     pub static ref INPUT: &'static str = include_str!("../../input");
 }
 
+#[cfg(not(feature = "input"))]
+pub const INPUT: &str = "";
+
 /// # Panics
-pub fn solve_1(_input: &str) -> u32 {
-    todo!()
+fn solve<const MIN_SAVING: usize, const CHEAT_LEN: usize>(input: &str) -> usize {
+    let map = input.as_bytes();
+    let width = map.iter().position(|&tile| tile == b'\n').unwrap();
+    let height = (map.len() + 1) / (width + 1);
+
+    let (start_r, start_c) = map
+        .chunks(width + 1)
+        .take(height)
+        .enumerate()
+        .find_map(|(r, row)| {
+            row.iter()
+                .take(width)
+                .position(|&tile| tile == b'S')
+                .map(|c| (r, c))
+        })
+        .unwrap();
+
+    let mut visited =
+        BitSet::<_, _, { BitSet::with_capacity(160 * 160) }>::new(|(r, c)| r * (width + 1) + c);
+    visited.insert((start_r, start_c)).unwrap();
+
+    let path = (0..).scan(((start_r, start_c), false), |((r, c), done), i| {
+        if *done {
+            return None;
+        }
+
+        if map[*r * (width + 1) + *c] == b'E' {
+            *done = true;
+            Some(((*r, *c), i))
+        } else {
+            let res = ((*r, *c), i);
+
+            (*r, *c) = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                .into_iter()
+                .find_map(
+                    |(dr, dc)| match (r.checked_add_signed(dr), c.checked_add_signed(dc)) {
+                        (Some(nr), Some(nc))
+                            if nr < height && nc < width && map[nr * (width + 1) + nc] != b'#' =>
+                        {
+                            if visited.insert((nr, nc)).unwrap() {
+                                None
+                            } else {
+                                Some((nr, nc))
+                            }
+                        }
+                        _ => None,
+                    },
+                )
+                .unwrap();
+
+            Some(res)
+        }
+    });
+    let mut main_path = Map::new();
+    for (k, v) in path {
+        main_path.insert(k, v).unwrap();
+    }
+
+    let path_len = main_path.len();
+
+    #[cfg(feature = "parallel")]
+    let steps = main_path.iter().par_bridge();
+
+    #[cfg(not(feature = "parallel"))]
+    let steps = main_path.iter();
+
+    let main_path = &main_path; // borrow checker :PPP
+    steps
+        .map(|(&(r, c), &i)| {
+            let (r, c) = (r, c);
+            (r.saturating_sub(CHEAT_LEN)..(r + CHEAT_LEN + 1).min(height))
+                .flat_map(move |tr| {
+                    (c.saturating_sub(CHEAT_LEN)..(c + CHEAT_LEN + 1).min(width)).filter_map(
+                        move |tc| {
+                            if (r, c) == (tr, tc) {
+                                return None;
+                            }
+
+                            let distance = r.abs_diff(tr) + c.abs_diff(tc);
+                            if distance <= CHEAT_LEN && map[tr * (width + 1) + tc] != b'#' {
+                                let cheat_path_len = path_len - main_path[&(tr, tc)] + i + distance;
+                                let saving = path_len.saturating_sub(cheat_path_len);
+                                if saving >= MIN_SAVING {
+                                    return Some((cheat_path_len, saving));
+                                }
+                            }
+
+                            None
+                        },
+                    )
+                })
+                .count()
+        })
+        .sum()
 }
 
 /// # Panics
-pub fn solve_2(_input: &str) -> u32 {
-    todo!()
+pub fn solve_1(input: &str) -> usize {
+    solve::<100, 2>(input)
 }
 
-pub fn part_1() -> u32 {
+/// # Panics
+pub fn solve_2(input: &str) -> usize {
+    solve::<100, 20>(input)
+}
+
+#[cfg(feature = "input")]
+pub fn part_1() -> usize {
     solve_1(&INPUT)
 }
 
-pub fn part_2() -> u32 {
+#[cfg(feature = "input")]
+pub fn part_2() -> usize {
     solve_2(&INPUT)
 }
 
@@ -28,18 +141,33 @@ pub fn part_2() -> u32 {
 mod tests {
     use super::*;
 
+    use lazy_static::lazy_static;
+
     lazy_static! {
-        static ref INPUT_1: &'static str = r#""#;
-        static ref INPUT_2: &'static str = r#""#;
+        static ref INPUT: &'static str = r#"###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############"#;
     }
 
     #[test]
     fn same_results_1() {
-        assert_eq!(solve_1(&INPUT_1), 42);
+        assert_eq!(solve::<12, 2>(&INPUT), 3 + 1 + 1 + 1 + 1 + 1);
     }
 
     #[test]
     fn same_results_2() {
-        assert_eq!(solve_2(&INPUT_2), 42);
+        assert_eq!(solve::<50, 20>(&INPUT), 285);
     }
 }
--- day20/rs/src/main.rs
+++ day20/rs/src/main.rs
@@ -1,17 +1,33 @@
 use std::time::Instant;
 
-use day20::{part_1, part_2};
-
+#[cfg(feature = "input")]
 fn main() {
     let now = Instant::now();
 
-    println!("part 1: {}", part_1());
-    println!("part 2: {}", part_2());
+    println!("part 1: {}", day20::part_1());
+    println!("part 2: {}", day20::part_2());
 
     let elapsed = now.elapsed();
     println!(
-        "elapsed: {}ms ({}ns)",
+        "elapsed: {}ms ({}us, {}ns)",
         elapsed.as_millis(),
+        elapsed.as_micros(),
+        elapsed.as_nanos()
+    );
+}
+
+#[cfg(not(feature = "input"))]
+fn main() {
+    let now = Instant::now();
+
+    println!("part 1: {}", day20::solve_1(&INPUT));
+    println!("part 2: {}", day20::solve_2(&INPUT));
+
+    let elapsed = now.elapsed();
+    println!(
+        "elapsed: {}ms ({}us, {}ns)",
+        elapsed.as_millis(),
+        elapsed.as_micros(),
         elapsed.as_nanos()
     );
 }
