f80a5c7a2d9b588481d3136e6cebf730050a9ecf
Vesco Claudio <claudio.vesco@gmail.com>
2024-12-30 01:13:03 +0100

    day16 on embedded

--- day16/rs/src/lib.rs
+++ day16/rs/src/lib.rs
@@ -3,9 +3,12 @@
 
 use core::cmp;
 
-use heapless::{binary_heap, BinaryHeap as HLBinaryHeap};
+use heapless::{binary_heap, BinaryHeap as HLBinaryHeap, Vec as HLVec};
 
 use bitset::BitSet;
+
+// mod arena;
+// pub use arena::*;
 
 #[cfg(feature = "input")]
 pub const INPUT: &str = include_str!("../../input");
@@ -13,11 +16,33 @@ pub const INPUT: &str = include_str!("../../input");
 #[cfg(not(feature = "input"))]
 pub const INPUT: &str = "";
 
+const DIRECTIONS: [(i8, i8); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+
 const WIDTH: usize = 140;
 const HEIGHT: usize = 140;
 const DIMENSION: usize = 5;
 
 type Queue<T> = HLBinaryHeap<T, binary_heap::Max, { 1024 * 4 }>;
+type Stack<T> = HLVec<T, 512>;
+
+#[derive(Debug, Eq, PartialEq)]
+struct Node {
+    cost: u32,
+    position: (u8, u8),
+    direction: (i8, i8),
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 #[allow(clippy::cast_sign_loss, clippy::cast_possible_wrap)]
 #[inline]
@@ -35,31 +60,12 @@ fn costs_key((r, c): (u8, u8), (dr, dc): (i8, i8)) -> usize {
 
 /// # Panics
 #[allow(clippy::cast_possible_truncation)]
-fn dijkstra(
+fn dijkstra_first(
     costs: &mut [u32],
     maze: &[u8],
     &(height, width): &(usize, usize),
     (start_r, start_c): (u8, u8),
 ) -> Option<(u32, (u8, u8))> {
-    #[derive(Debug, Eq, PartialEq)]
-    struct Node {
-        cost: u32,
-        position: (u8, u8),
-        direction: (i8, i8),
-    }
-
-    impl Ord for Node {
-        fn cmp(&self, other: &Self) -> cmp::Ordering {
-            other.cost.cmp(&self.cost)
-        }
-    }
-
-    impl PartialOrd for Node {
-        fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-            Some(self.cmp(other))
-        }
-    }
-
     let mut queue = Queue::new();
     queue
         .push(Node {
@@ -83,7 +89,7 @@ fn dijkstra(
             return Some((cost, (r, c)));
         }
 
-        for (ndr, ndc) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+        for (ndr, ndc) in DIRECTIONS {
             match (r.checked_add_signed(ndr), c.checked_add_signed(ndc)) {
                 (Some(r), Some(c))
                     if (r as usize) < height
@@ -122,6 +128,79 @@ fn dijkstra(
 
 /// # Panics
 #[allow(clippy::cast_possible_truncation)]
+#[allow(dead_code)]
+fn dijkstra_full(
+    costs: &mut [u32],
+    maze: &[u8],
+    &(height, width): &(usize, usize),
+    (start_r, start_c): (u8, u8),
+) -> Option<(u32, (u8, u8))> {
+    let mut result = None;
+
+    let mut queue = Queue::new();
+    queue
+        .push(Node {
+            position: (start_r, start_c),
+            direction: (0, 1),
+            cost: 0,
+        })
+        .unwrap();
+
+    while let Some(Node {
+        position: (r, c),
+        direction: (dr, dc),
+        cost,
+    }) = queue.pop()
+    {
+        if cost > costs[costs_key((r, c), (dr, dc))] {
+            continue;
+        }
+
+        if maze[r as usize * (width + 1) + c as usize] == b'E' {
+            if result.is_none() {
+                result = Some((cost, (r, c)));
+            }
+            continue;
+        }
+
+        for (ndr, ndc) in DIRECTIONS {
+            match (r.checked_add_signed(ndr), c.checked_add_signed(ndc)) {
+                (Some(r), Some(c))
+                    if (r as usize) < height
+                        && (c as usize) < width
+                        && maze[r as usize * (width + 1) + c as usize] != b'#' =>
+                {
+                    let neighbor_cost = if (ndr, ndc) == (dr, dc) {
+                        0
+                    } else if ndr == -dr || ndc == -dc {
+                        continue;
+                    } else {
+                        1000
+                    } + cost
+                        + 1;
+
+                    let e = &mut costs[costs_key((r, c), (ndr, ndc))];
+                    if *e >= neighbor_cost {
+                        *e = neighbor_cost;
+                        queue
+                            .push(Node {
+                                position: (r, c),
+                                direction: (ndr, ndc),
+                                cost: neighbor_cost,
+                            })
+                            .unwrap();
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    result
+}
+
+/// # Panics
+#[allow(clippy::cast_possible_truncation)]
 pub fn solve_1(input: &str) -> u32 {
     let maze = input.as_bytes();
     let width = maze.iter().position(|&c| c == b'\n').unwrap();
@@ -142,7 +221,7 @@ pub fn solve_1(input: &str) -> u32 {
     let mut costs = [u32::MAX; { WIDTH * HEIGHT * DIMENSION }];
     costs[costs_key((start_r as u8, start_c as u8), (0i8, 1i8))] = 0u32;
 
-    let (cost, _) = dijkstra(
+    let (cost, _) = dijkstra_first(
         &mut costs,
         maze,
         &(height, width),
@@ -156,64 +235,6 @@ pub fn solve_1(input: &str) -> u32 {
 /// # Panics
 #[allow(clippy::cast_possible_truncation)]
 pub fn solve_2(input: &str) -> usize {
-    struct Visit<'a, T, K, const N: usize> {
-        set: &'a mut BitSet<T, K, N>,
-        visited: &'a mut [u32],
-        maze: &'a [u8],
-        dim: (usize, usize),
-        end: (u8, u8),
-        best_cost: u32,
-    }
-
-    impl<K, const N: usize> Visit<'_, (u8, u8), K, N>
-    where
-        K: Fn(&(u8, u8)) -> usize,
-    {
-        fn visit(&mut self, (r, c): (u8, u8), (dr, dc): (i8, i8), cost: u32) -> bool {
-            if (r, c) == self.end {
-                return true;
-            }
-
-            let (height, width) = self.dim;
-
-            let mut found = false;
-            for (ndr, ndc) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                match (r.checked_add_signed(ndr), c.checked_add_signed(ndc)) {
-                    (Some(r), Some(c))
-                        if (r as usize) < height
-                            && (c as usize) < width
-                            && self.maze[r as usize * (width + 1) + c as usize] != b'#' =>
-                    {
-                        let neighbor_cost = if (ndr, ndc) == (dr, dc) {
-                            0
-                        } else if ndr == -dr || ndc == -dc {
-                            continue;
-                        } else {
-                            1000
-                        } + cost
-                            + 1;
-
-                        if neighbor_cost > self.best_cost {
-                            continue;
-                        }
-
-                        let e = &mut self.visited[costs_key((r, c), (ndr, ndc))];
-                        if *e >= neighbor_cost {
-                            *e = neighbor_cost;
-                            if self.visit((r, c), (ndr, ndc), neighbor_cost) {
-                                self.set.insert((r, c)).unwrap();
-                                found = true;
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            }
-
-            found
-        }
-    }
-
     let maze = input.as_bytes();
     let width = maze.iter().position(|&c| c == b'\n').unwrap();
     let height = (maze.len() + 1) / (width + 1);
@@ -233,7 +254,7 @@ pub fn solve_2(input: &str) -> usize {
     let mut costs = [u32::MAX; { WIDTH * HEIGHT * DIMENSION }];
     costs[costs_key((start_r as u8, start_c as u8), (0i8, 1i8))] = 0u32;
 
-    let (best_cost, (end_r, end_c)) = dijkstra(
+    let (best_cost, (end_r, end_c)) = dijkstra_first(
         &mut costs,
         maze,
         &(height, width),
@@ -241,22 +262,64 @@ pub fn solve_2(input: &str) -> usize {
     )
     .unwrap();
 
+    visit_back(
+        &costs,
+        &(height, width),
+        (end_r, end_c),
+        best_cost,
+        (start_r as u8, start_c as u8),
+    )
+}
+
+fn visit_back(
+    costs: &[u32],
+    &(_, width): &(usize, usize),
+    (end_r, end_c): (u8, u8),
+    best_cost: u32,
+    (start_r, start_c): (u8, u8),
+) -> usize {
     let mut set =
         BitSet::<(u8, u8), _, { BitSet::with_capacity(WIDTH * HEIGHT) }>::new(|&(r, c)| {
             (r - 1) as usize * width + (c - 1) as usize
         });
 
-    let mut visit = Visit {
-        set: &mut set,
-        visited: &mut costs,
-        maze,
-        dim: (height, width),
-        end: (end_r, end_c),
-        best_cost,
-    };
+    let mut stack = Stack::new();
+    for (dr, dc) in DIRECTIONS {
+        if costs[costs_key((end_r, end_c), (dr, dc))] == best_cost {
+            stack.push(((end_r, end_c), (dr, dc), best_cost)).unwrap();
+        }
+    }
 
-    if visit.visit((start_r as u8, start_c as u8), (0, 1), 0) {
-        set.insert((start_r as u8, start_c as u8)).unwrap();
+    while let Some(((r, c), (dr, dc), cost)) = stack.pop() {
+        set.insert((r, c)).unwrap();
+
+        let (r, c) = (
+            r.checked_add_signed(-dr).unwrap(),
+            c.checked_add_signed(-dc).unwrap(),
+        );
+        for (ndr, ndc) in DIRECTIONS {
+            let Some(cost) = cost
+                .checked_sub(if (ndr, ndc) == (dr, dc) {
+                    0
+                } else if ndr == -dr || ndc == -dc {
+                    continue;
+                } else {
+                    1000
+                })
+                .and_then(|value| value.checked_sub(1))
+            else {
+                continue;
+            };
+
+            if (r, c) == (start_r, start_c) {
+                set.insert((r, c)).unwrap();
+                continue;
+            }
+
+            if costs[costs_key((r, c), (ndr, ndc))] == cost {
+                stack.push(((r, c), (ndr, ndc), cost)).unwrap();
+            }
+        }
     }
 
     set.len()
